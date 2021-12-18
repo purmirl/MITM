bce59ca16642705abdda45ad135a84d40e3f69f0
purmirl <gmlrnjs1994@gmail.com>
2021-12-18 17:44:50 +0900

    20211218-01 : making cui

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+ MITM (Man In The Middle) by PeTrA 2021 ~
+ ARP spoofing Architecture
+    . base of Python3.8.2, Scapy2.4.3
+
+ @ main.py
+    * MITM/main.py
+"""
+
+""" @ main function
+"""
+
+def main():
+    return
+
+""" @ call main
+"""
+if __name__ == "__main__":
+    main()
---
+++ src/package/cui.py
@@ -0,0 +1,16 @@
+"""
+ MITM (Man In The Middle) by PeTrA 2021 ~
+ ARP spoofing Architecture
+    . base of Python3.8.2, Scapy2.4.3
+
+ @ cui.py
+    * MITM/src/package/cui.py
+"""
+
+""" @ Cui class
+"""
+class Cui:
+    def __init__(self):
+        return
+
+    def cui_egine(self):
