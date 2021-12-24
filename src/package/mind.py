a3cad6dfdcb71d3efc238a1fe5359995941743de
purmirl <gmlrnjs1994@gmail.com>
2021-12-24 20:27:08 +0900

    20211224-01 : mind.py is added.

---
+++ src/package/mind.py
@@ -0,0 +1,22 @@
+"""
+ MITM (Man In The Middle) by PeTrA 2021 ~
+ ARP spoofing Architecture
+    . base of Python3.8.2, Scapy2.4.3
+
+ @ cui.py
+    * MITM/src/package/mind.py
+"""
+
+""" @ Mind class
+"""
+class Mind:
+    def __init__(self):
+        self.reset_value()
+        return
+
+    def reset_value(self):
+        return
+
+    def get_local_area_network_information(self):
+
+        return
