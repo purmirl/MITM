c1ac2ce058b0ed7a83e765afb41a76b9ffd9862f
purmirl <gmlrnjs1994@gmail.com>
2022-01-01 18:41:30 +0900

    20220101-01 : get gateway information making...

--- src/package/mind.py
+++ src/package/mind.py
@@ -6,7 +6,12 @@
  @ cui.py
     * MITM/src/package/mind.py
 """
+import scapy
+from scapy.arch import IFACES
+from scapy.compat import raw
 from scapy.config import conf
+from scapy.layers.inet import IP, ICMP
+from scapy.sendrecv import sr1
 
 """ @ Mind class
 """
@@ -23,8 +28,9 @@ class Mind:
         return
 
     def get_local_area_network_information(self):
-        gateway_ip_address = conf.route.route("0.0.0.0")[2]
-
-        print("gateway ip address : " + str(gateway_ip_address))
+        get_gateway_information_packet = sr1(IP(dst = "8.8.8.8", ttl = 0) / ICMP(), verbose = 0)
+        gateway_ip_address = get_gateway_information_packet.src
+        print(get_gateway_information_packet.show())
+        # print("gateway ip address : " + str(gateway_ip_address))
 
         return gateway_ip_address
