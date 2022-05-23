83bddb29505a631c20fc2e623c33a9400a5eeb37
purmirl <gmlrnjs1994@gmail.com>
2022-05-23 20:56:43 +0900

    20220523-01 : renewal

--- src/package/mind.py
+++ src/package/mind.py
@@ -6,31 +6,43 @@
  @ cui.py
     * MITM/src/package/mind.py
 """
-import scapy
-from scapy.arch import IFACES
-from scapy.compat import raw
-from scapy.config import conf
 from scapy.layers.inet import IP, ICMP
+from scapy.layers.l2 import ARP, Ether
 from scapy.sendrecv import sr1
 
-""" @ Mind class
-"""
-class Mind:
-    def __init__(self):
-        self.reset_value()
-        return
 
-    def reset_value(self):
-        return
+def arp_spoofing(_target_ip_address):
+    target_ip_address = _target_ip_address
+    local_area_network_gateway_ip_address = get_local_area_network_gateway_ip_address()
+    local_area_network_gateway_mac_address = get_ip_mac_address(local_area_network_gateway_ip_address)
 
-    def mind_engine(self):
-        self.get_local_area_network_information()
-        return
 
-    def get_local_area_network_information(self):
-        get_gateway_information_packet = sr1(IP(dst = "8.8.8.8", ttl = 0) / ICMP(), verbose = 0)
-        gateway_ip_address = get_gateway_information_packet.src
-        print(get_gateway_information_packet.show())
-        # print("gateway ip address : " + str(gateway_ip_address))
 
-        return gateway_ip_address
+
+    return
+
+
+def get_local_area_network_gateway_ip_address():
+
+    return
+
+def get_local_area_network_information(self):
+    get_gateway_information_packet = sr1(IP(dst = "8.8.8.8", ttl = 0) / ICMP(), verbose = 0)
+    gateway_ip_address = get_gateway_information_packet.src
+    print("gateway ip address : " + str(gateway_ip_address))
+
+    self.get_target_mac_address(str(gateway_ip_address))
+
+    return gateway_ip_address
+
+
+def get_ip_mac_address(_ip_address):
+    arp_response_packet = sr1(Ether(dst = "ff:ff:ff:ff:ff:ff") / ARP(pdst = str(_ip_address)), verbose = 0)
+    result = arp_response_packet[0][1][ARP].hwsrc
+    print(result)
+    return result
+
+
+def get_my_mac_address(self):
+
+    return
